cfd637c4e9f6b9d6c3fceef82f99997d2128a05b
Katherin-Chuco <katherin.ch.20@gmail.com>
2020-05-26 23:03:42 -0500

    Add metatags

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,15 +5,26 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+
 
         <title>AR BeastBox</title>
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://arbeastbox.ml/" />
+        <meta property="og:image" content="../images/patterns/back-2.png" />
+        <meta property="og:title" content="AR BeastBox" />
+        <meta name="description" content="Es tiempo de llevar tus beats a otra realidad." />
+        <meta property="og:description" content="Es tiempo de llevar tus beats a otra realidad." />
+        <meta author="AR BeastBox" />
+
 
         <link rel="shortcut icon" href="favicon.ico">
         <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
         <!-- FontAwesome JS-->
         <script defer src="https://use.fontawesome.com/releases/v5.8.0/js/all.js" integrity="sha384-ukiibbYjFS/1dhODSWD+PrZ6+CGCgf8VbyUH7bQQNUulL+2r59uGYToovytTf4Xm" crossorigin="anonymous"></script>
+
 
 
         <!-- Global CSS -->
